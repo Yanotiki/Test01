a7eab4a6fad80a36b43e9b5d133b648fd2c1ec5a
Yanotiki <yana.fedorenko@soft-machine.ru>
2021-10-13 12:11:40 +0300

    ObjedMass2

--- src/test/java/ObjedMas2.java
+++ src/test/java/ObjedMas2.java
@@ -4,7 +4,7 @@ package com.javarush.task.pro.task05.task0504;
 Объединяем массивы
 */
 
-public class ObjedMas {
+public class ObjedMas2 {
     public static int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     public static int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
     public static int[] resultArray;
